6f2ca6e6fdc64ba03b6c023c9064ab61abf90920
Isabela_ <isabela.rosero@correounivalle.edu.co>
2024-06-27 12:32:59 -0500

    user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,97 +1,151 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { styled } from '@mui/material/styles';
-import { InputBase, IconButton} from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { Link, useNavigate } from 'react-router-dom';
-import { Filtro5 } from '../service/data';
-import logo from '/src/assets/logovalle.png';
+// src/components/Header.js
+import React, { useEffect, useRef, useState } from "react";
+import { styled } from "@mui/material/styles";
+import { InputBase } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link, useNavigate } from "react-router-dom";
+import { Filtro5 } from "../service/data";
+import logo from "/src/assets/logounivalle.svg";
 
-const HeaderContainer = styled('div')({
-  display: 'flex',
-  flexDirection: 'row',
-  placeItems: 'center',
-  justifyContent: 'space-between',
-  width: '100%',
-  position: 'sticky', 
-  top: 0, 
-  marginBottom: '60px',
+const HeaderContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: "100%",
+  height: "80px",
+  position: "sticky",
+  top: 0,
+  marginBottom: "60px",
   zIndex: 2,
-  backgroundColor: '#F2F2F2',
-  boxShadow: '1px 2px 6px #BFB9A6',
-  '@media (max-width:600px)': {
-    flexDirection: 'column', 
-  },
-});
-
-const Title = styled('div')(({ theme }) => ({
-  textAlign: 'center',
-  fontWeight: 500,
-  fontSize: '35px',
-  color: '#423b3b',
-  [theme.breakpoints.down('sm')]: {
-    paddingTop: 0,
-    fontSize: '23px',
+  backgroundColor: "#F2F2F2",
+  boxShadow: "0px 14px 20px -17px rgba(66, 68, 90, 1)",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    alignItems: "center",
+    height: "auto",
   },
 }));
-
-const SearchBar = styled('div')({
-  position: 'relative',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end', 
-  paddingRight: '10px',
-});
-
-const SearchInput = styled(InputBase)({
-  marginRight: '0.5rem',
-  border: '1px solid #ccc', 
-  borderRadius: '5px', 
-  paddingLeft: '12px',
-  width:"290px",
-});
-
-const SearchResultLabel = styled('label')({
-  padding: '0.5rem',
-  border: '1px solid #ccc',
-  marginTop: '-10px',
-  marginRight: '20px',
-  borderRadius: '5px',
-  backgroundColor: '#ffffff',
-  cursor: 'pointer',
-  width:'300px',
-  alignSelf:'flex-end',
-  position: 'absolute', 
-  top: 'calc(100% + 10px)',
-  //top: '100%', 
-  //right: 0,
+const TitleContainer = styled("div")(({ theme }) => ({
+  position: "absolute",
+  left: "50%",
+  transform: "translateX(-50%)",
+  whiteSpace: "nowrap",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  [theme.breakpoints.down("sm")]: {
+    position: "static",
+    transform: "none",
+  },
+}));
+const Title = styled("div")(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: "30px",
+  color: "#423b3b",
+  fontFamily: "Helvetica, sans-serif",
+  [theme.breakpoints.down("sm")]: {
+    textAlign: "center",
+    margin: "10px 0",
+  },
+}));
+const SearchBar = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  position: "relative",
+  paddingRight: "20px",
+  [theme.breakpoints.down("sm")]: {
+    paddingRight: 0,
+    width: "100%",
+    justifyContent: "center",
+  },
+}));
+const SearchInputContainer = styled("div")(({ theme }) => ({
+  position: "relative",
+  width: "300px",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+    maxWidth: "300px",
+  },
+}));
+const SearchInput = styled(InputBase)(({ theme }) => ({
+  width: "100%",
+  paddingRight: "20px",
+  border: "1px solid #ccc",
+  borderRadius: "20px",
+  paddingLeft: "12px",
+  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+  "&:focus": {
+    borderColor: "#007BFF",
+    boxShadow: "0px 4px 8px rgba(0, 123, 255, 0.2)",
+  },
+}));
+const StyledSearchIcon = styled(SearchIcon)(({ theme }) => ({
+  position: "absolute",
+  right: "4px",
+  top: "50%",
+  transform: "translateY(-50%)",
+  pointerEvents: "none",
+  color: "#808181",
+  borderRadius: "100px",
+  fontSize: "27px",
+}));
+const Username = styled("div")(({ theme }) => ({
+  textAlign: "center",
+  fontWeight: 500,
+  paddingRight: "10px",
+  paddingLeft: "10px",
+  fontFamily: "Helvetica, sans-serif",
+  [theme.breakpoints.down("sm")]: {
+    marginTop: "10px",
+    flexDirection: "column-reverse",
+  },
+}));
+const SearchResultLabel = styled("label")(({ theme }) => ({
+  padding: "0.2rem",
+  border: "1px solid #ccc",
+  marginTop: "-6px",
+  marginRight: "20px",
+  borderRadius: "5px",
+  backgroundColor: "#ffffff",
+  cursor: "pointer",
+  width: "300px",
+  alignSelf: "flex-end",
+  position: "absolute",
+  right: 0,
+  top: "calc(100% + 10px)",
   zIndex: 3,
-  maxHeight: '200px', 
-  overflowY: 'auto',
-  display: 'none', 
-  '&::-webkit-scrollbar': {
-    width: '8px',
+  maxHeight: "200px",
+  overflowY: "auto",
+  display: "none",
+  "&::-webkit-scrollbar": {
+    width: "8px",
   },
-  '&::-webkit-scrollbar-thumb': {
-    backgroundColor: '#888',
-    borderRadius: '4px',
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: "#888",
+    borderRadius: "4px",
   },
-  '&::-webkit-scrollbar-track': {
-    backgroundColor: '#f0f0f0',
-    borderRadius: '4px',
+  "&::-webkit-scrollbar-track": {
+    backgroundColor: "#f0f0f0",
+    borderRadius: "4px",
   },
-});
-
-const SearchResultItem = styled('div')({
-  padding: '0.5rem',
-  '&:hover': {
-    backgroundColor: '#d3d3d3',
+}));
+const SearchResultItem = styled("div")(({ theme }) => ({
+  padding: "0.5rem",
+  "&:hover": {
+    backgroundColor: "#d3d3d3",
   },
-});
+}));
+const Logo = styled("div")(({ theme }) => ({
+  paddingLeft: "10px",
+  display: "flex",
+}));
 
 const Header = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [isLabelVisible, setIsLabelVisible] = useState(false);
+  const [user, setUser] = useState('');
+  const [isCargo, setCargo] = useState(['']);
   const navigate = useNavigate();
   const labelRef = useRef(null);
 
@@ -101,34 +155,40 @@ const Header = () => {
 
   const handleSearch = async () => {
     try {
-      if (!searchTerm || typeof searchTerm !== 'string') {
+      if (!searchTerm || typeof searchTerm !== "string") {
         setSearchResults([]);
         setIsLabelVisible(false);
         return;
       }
-  
+
       const academicPrograms = await Filtro5();
       if (!academicPrograms || !Array.isArray(academicPrograms)) {
-        console.error('academicPrograms no es una matriz válida');
+        console.error("academicPrograms no es una matriz válida");
         return;
       }
-  
+
       const searchTermNormalized = searchTerm.toLowerCase().trim();
-      const results = academicPrograms.filter(programa =>
-        programa['programa académico'] && removeAccents(programa['programa académico'].toLowerCase()).includes(searchTermNormalized)
+      const results = academicPrograms.filter(
+        (programa) =>
+          programa["programa académico"] &&
+          removeAccents(programa["programa académico"].toLowerCase()).includes(
+            searchTermNormalized
+          )
       );
-  
-      setSearchResults(results); 
+
+      setSearchResults(results);
       setIsLabelVisible(true);
     } catch (error) {
-      console.error('Error obteniendo los datos de los programas académicos:', error);
+      console.error(
+        "Error obteniendo los datos de los programas académicos:",
+        error
+      );
     }
   };
-  
 
   const handleResultClick = (result) => {
-    navigate('/program_details', { state: result });
-    setSearchTerm('');
+    navigate("/program_details", { state: result });
+    setSearchTerm("");
     setSearchResults([]);
     setIsLabelVisible(false);
   };
@@ -139,17 +199,37 @@ const Header = () => {
     }
   };
 
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  };
+
+  const extractNameFromEmail = (email) => {
+    const name = email.split('@')[0];
+    const [firstName] = name.split('.');
+    return capitalizeFirstLetter(firstName);
+  };
+
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutsideLabel);
+    document.addEventListener("mousedown", handleClickOutsideLabel);
+
+    if (sessionStorage.getItem('logged')) {
+      let res = JSON.parse(sessionStorage.getItem('logged'));
+      const permisos = res.map(item => item.permiso).flat();
+      setCargo(permisos);
+      const email = res[0].user;
+      const userName = extractNameFromEmail(email);
+      setUser(userName);
+      console.log("Permisos del usuario:", permisos);
+    }
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutsideLabel);
+      document.removeEventListener("mousedown", handleClickOutsideLabel);
     };
   }, []);
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
-    if (e.target.value === '') {
+    if (e.target.value === "") {
       setSearchResults([]);
       setIsLabelVisible(false);
     } else {
@@ -158,31 +238,44 @@ const Header = () => {
   };
 
   return (
-    <HeaderContainer>
-      <Link to="/">
-        <div className='logo' style={{paddingLeft:'10px'}}><img src={logo} alt="Logo" /></div>
-      </Link>
-      <Title>Sistema SIAC Facultad de Salud</Title>
+    <HeaderContainer role="navegación" aria-label="Navegación Principal">
+      <Logo>
+        <Link to="/" alt="Logo Universidad del Valle" aria-label="Homepage de Sistema Siac">
+          <img src={logo} loading="lazy" />
+        </Link>
+      </Logo>
+      <TitleContainer>
+        <Title>Sistema SIAC Facultad de Salud</Title>
+        <Username>
+          {user ? `${user} - ${isCargo.join(', ')}` : "Cargando..."}
+        </Username>
+      </TitleContainer>
       <SearchBar>
-        <SearchInput 
-          placeholder="Buscar..." 
-          value={searchTerm} 
-          onClick={() => setIsLabelVisible(true)}
-          onChange={handleInputChange}
-        />
-        <IconButton type="submit" aria-label="search" onClick={handleSearch}>
-          <SearchIcon />
-        </IconButton>
-        <SearchResultLabel ref={labelRef} style={{ display: isLabelVisible ? 'block' : 'none' }}>
+        <SearchInputContainer>
+          <SearchInput
+            placeholder="Buscar..."
+            value={searchTerm}
+            onClick={() => setIsLabelVisible(true)}
+            onChange={handleInputChange}
+          />
+          <StyledSearchIcon />
+        </SearchInputContainer>
+        <SearchResultLabel
+          ref={labelRef}
+          style={{ display: isLabelVisible ? "block" : "none" }}
+        >
           {searchResults.map((result, index) => (
-            <SearchResultItem key={index} onClick={() => handleResultClick(result)}>
-              {result['programa académico']}
+            <SearchResultItem
+              key={index}
+              onClick={() => handleResultClick(result)}
+            >
+              {result["programa académico"]}
             </SearchResultItem>
           ))}
         </SearchResultLabel>
       </SearchBar>
     </HeaderContainer>
   );
-}
+};
 
 export default Header;
